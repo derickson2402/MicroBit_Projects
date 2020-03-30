73a873e8b6c614ff489a5ce7ad9b1b2436c3e08e
Daniel Erickson <derickson2402@gmail.com>
2020-03-29 20:37:54 -0400

    Create branch, change MultiPin Playback function, create README
    Previously, MultiPin_Playback created major memory overflow errors whenever more than ~10 notes existed in a song. This was due to the 8kB usable RAM on the micro:bit, which couldn't contain the 100n, 24kB per note array created by the Create_Millisecond_Music function.
    
    Obviously the function was extremely inefficient, so instead the Play_Dual_Soundfunction was changed to simply play one given note n times. Create_Millisecond_Music was scrapped in favor of Create_Single_Step_List, which just expands the given list so each note is only length 1. This, too, originally proved to overload the memory, so the function also deletes the original list as it goes, to save on memory.
    
    The program also suffered from playback timing issues, likely due to the BPM of 600, and Tick length of 100. The music module was probably never made with that in mind, so it didn't seem to work well. I simply took it down an order of magnitude, and shortened the play duration.

--- Music_Project/Music_Player.py
+++ Music_Project/Music_Player.py
@@ -1,36 +1,35 @@
 from microbit import *
 import music
 
-MegaMelody = ["D4:1", "D4:1", "D5:1", "R:1", "A4:1", "R:1", "G#4:2", "G4:2", "F4:3", "D4:1", "F4:1", "G4:1", \
+#Ticks=16, BPM=30
+Megalovania = ["D4:1", "D4:1", "D5:1", "R:1", "A4:1", "R:1", "G#4:2", "G4:2", "F4:3", "D4:1", "F4:1", "G4:1", \
 "C4:1", "C4:1", "D5:1", "R:1", "A4:1", "R:1", "G#4:2", "G4:2", "F4:3", "D4:1", "F4:1", "G4:1", \
 "B3:1", "B3:1", "D5:1", "R:1", "A4:1", "R:1", "G#4:2", "G4:2", "F4:3", "D4:1", "F4:1", "G4:1", \
 "A#3:1", "A#3:1", "D5:1", "R:1", "A4:1", "R:1", "G#4:2", "G4:2", "F4:3", "D4:1", "F4:1", "G4:1"]
 
-def Play_Megalovania():
-    music.set_tempo(ticks=16, bpm=30)
-    display.show(Image.HAPPY)
-    music.play(MegaMelody)
-
-RunawayMelody = ["C6:3", "R:1", "C6:3", "R:1", "C6:3", "R:1", "C5:3", "R:1", \
+#Ticks=8, BPM=25
+Runaway = ["C6:3", "R:1", "C6:3", "R:1", "C6:3", "R:1", "C5:3", "R:1", \
 "B5:3", "R:1", "B5:3", "R:1", "B5:3", "R:1", "B4:3", "R:1", \
 "A5:3", "R:1", "A5:3", "R:1", "A5:3", "R:1", "A4:3", "R:1", \
 "F5:3", "R:1", "F5:3", "R:1", "E5:3", "R:1", "C6:3"]
 
-def Play_Runaway():
-    music.set_tempo(ticks=8, bpm=25)
-    display.show(Image.SILLY)
-    music.play(RunawayMelody)
+def Play_Song(Song, Ticks, BPM, Display_Image):
+    display.show(Display_Image)
+    print("Now playing:", str(Song), ", arranged by Daniel Erickson!")
+    music.set_tempo(ticks=Ticks, bpm=BPM)
+    music.play(Song, pin=pin0)
 
 def End_Program():
     display.show(Image("12345:34567:56789:78901:90123"))
     for freq in range(3000, 400, -50):
         music.pitch(freq, 1)
-    sleep(500)
+    sleep(1000)
+    quit("The program has been terminated\n\nGoodbye!")
 
 ProgramRunning = True
 while (ProgramRunning == True):
     if (button_a.is_pressed()):
-        Play_Megalovania()
+        Play_Song(Megalovania, 16, 30, Image.HAPPY)
     elif (button_b.is_pressed()):
-        Play_Runaway()
+        Play_Song(Runaway, 8, 25, Image.SILLY)
     display.clear()
---
+++ Music_Project/README.md
@@ -0,0 +1,30 @@
+# Music Project
+
+These programs utilize the built-in music modules on the micro:bit, which allows for audio playback using PWM on any 1 of the 3 pins. Simply pass a list where each note is a string, set up like:
+
+```
+"Note Name" + "Octave" + ":" + "Ticks" = "C4:4"
+```
+
+**Please note** that Python lists take up ~100 bytes of memory, and each element adds ~24 bytes. The micro:bit only has 16kB of onboard RAM, about 8kB of which is actually usable. Therefore, not many songs are preloaded in the software, as memory allocation errors are easy to produce.
+
+## Music_Player
+
+This program simply plays whatever song you feed it. Built in songs include:
+
+- Runaway - by Kanye West
+- Megalovania - from Undertale
+
+Create your own list in the software, plug it in to the function, and have some fun!
+
+## MultiPin_Playback
+
+This is where the fun begins!
+
+The MultiPin_Playback program allows two independent songs to be played simultaneously by the microbit! Normally, the microbit can only output audio to one pin at a time, and cannot run parallel processes unless you use Java and have way more patience than me. By alternating between two songs as fast as possible (about 1ms), both songs appear to play simultaneously. It sounds kinda gross, but hey, it's kinda neat.
+
+**TL:DR**: I spent way too much time making a mediocre solution to play 2 songs at the same time. Enjoy!
+
+The program is largely intended for playing a harmony and a melody, and for logistic reasons the songs must be the same length or else an error will occur.
+
+Other than that, simply enter both songs, the ticks per beat, and the BPM of the song to get started.
